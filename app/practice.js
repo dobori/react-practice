9e12307a16afd5b34821cc5dd44ee842f92bdab7
Borbala Dobo <dobori@gmail.com>
2017-11-29 13:08:02 +0100

    feat(react-practice): first line with react is ready

--- app/practice.js
+++ app/practice.js
@@ -1,1 +1,15 @@
-alert('ohjeee');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var createReactClass = require('create-react-class');
+
+var WhatToSee = createReactClass({
+
+    render:function(){
+        return(
+            <h1>yeeeee</h1>
+        );
+    } 
+
+});
+
+ReactDOM.render(<WhatToSee />, document.getElementById('show_stuffs'));
