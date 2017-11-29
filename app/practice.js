e6e5607d5bc3872cea90df73300ab831fa7778ea
Borbala Dobo <dobori@gmail.com>
2017-11-29 16:38:11 +0100

    feat(react-practice): starting tic-tac-toe and draw out the board

--- app/practice.js
+++ app/practice.js
@@ -2,14 +2,89 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var createReactClass = require('create-react-class');
 
-var WhatToSee = createReactClass({
+// var WhatToSee = createReactClass({
 
-    render:function(){
+//     render:function(){
+//         return(
+//             <div>
+//                 <h1>yeeeee</h1>
+//                 <p>valami</p>
+//                 <p>{this.props.msg}</p>
+//             </div>
+//         );
+//     } 
+
+// });
+
+// class ShoppingList extends React.Component {
+//     render() {
+//       return (
+//         <div className="shopping-list">
+//           <h1>Shopping List for {this.props.name}</h1>
+//           <ul>
+//             <li>Instagram</li>
+//             <li>WhatsApp</li>
+//             <li>Oculus</li>
+//           </ul>
+//         </div>
+//       );
+//     }
+//   }
+
+class Square extends React.Component {
+    render() {
         return(
-            <h1>yeeeee</h1>
-        );
-    } 
+            <button onClick={() => alert(this.props.value)}>{this.props.value}</button>
+        )
+    }
+}
 
-});
+class Board extends React.Component {
+    renderSquare(i) {
+        return(
+            <Square value={i} />
+        )
+    }
 
-ReactDOM.render(<WhatToSee />, document.getElementById('show_stuffs'));
+    render() {
+        const status = 'Next player: X';
+
+        return (
+            <div>
+              <div className="status">{status}</div>
+              <div className="board-row">
+                {this.renderSquare(0)}
+                {this.renderSquare(1)}
+                {this.renderSquare(2)}
+              </div>
+              <div className="board-row">
+                  {this.renderSquare(3)}
+                  {this.renderSquare(4)}
+                  {this.renderSquare(5)}
+              </div>
+              <div className="board-row">
+                  {this.renderSquare(6)}
+                  {this.renderSquare(7)}
+                  {this.renderSquare(8)}
+              </div>
+            </div>
+        )
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div>
+                <Board />
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        )
+    }
+}
+
+
+ReactDOM.render(<Game />, document.getElementById('show_stuffs'));
