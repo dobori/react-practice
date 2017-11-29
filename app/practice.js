5e9116b4f92e400ad43b8eb7be253713d20e8ad2
Borbala Dobo <dobori@gmail.com>
2017-11-29 17:58:38 +0100

    feat(react-practice): buttons are changeing status

--- app/practice.js
+++ app/practice.js
@@ -30,20 +30,39 @@ var createReactClass = require('create-react-class');
 //       );
 //     }
 //   }
-
-class Square extends React.Component {
-    render() {
-        return(
-            <button onClick={() => alert(this.props.value)}>{this.props.value}</button>
-        )
-    }
-}
+function Square(props) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
+  }
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          squares: Array(9).fill(null),
+          xIsNext: true,
+        };
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+          squares: squares,
+          xIsNext: !this.state.xIsNext,
+        });
+    }
+
     renderSquare(i) {
-        return(
-            <Square value={i} />
-        )
+        return (
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
     }
 
     render() {
